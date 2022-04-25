d5bcfdc763a11e1e4ff0e860d912d38906fb558b
Robbie <robbie@reformcollective.com>
2022-04-25 10:02:25 -0600

    Rename some things for consistency

--- src/pages/using-ssr.tsx
+++ src/pages/using-ssr.tsx
@@ -1,8 +1,8 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/Layout_"
+import Seo from "../components/Seo_"
 
 const UsingSSR = ({ serverData }) => {
   return (
