5638cf1e8d0b1af65a76e310e8c468fc31399f78
Robbie <robbie@reformcollective.com>
2022-04-25 11:16:07 -0600

    now builds!

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,103 +1,139 @@
 import type { GatsbyConfig } from "gatsby"
+import path from "path"
 
 const config: GatsbyConfig = {
-  //TODO set site metadata
+  // TODO add metadata
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Reform Starter`,
+    siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
-    {
-      resolve: "gatsby-source-contentful",
-      options: {
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-      },
-    },
+    // TODO setup a contentful space
+    // {
+    //   resolve: "gatsby-source-contentful",
+    //   options: {
+    //     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    //     spaceId: process.env.CONTENTFUL_SPACE_ID,
+    //   },
+    // },
     "gatsby-plugin-styled-components",
-    {
-      resolve: `gatsby-plugin-typescript`,
-      options: {
-        isTSX: true, // defaults to false
-        jsxPragma: `jsx`, // defaults to "React"
-        allExtensions: true, // defaults to false
-      },
-    },
-    {
-      resolve: `gatsby-plugin-layout`,
-      options: {
-        component: require.resolve(`./src/components/Providers.tsx`),
-      },
-    },
-    {
-      resolve: `gatsby-plugin-svgr`,
-      options: {
-        prettier: true,
-        svgo: true,
-        memo: true,
-        ref: true,
-        svgoConfig: {
-          plugins: [
-            {
-              name: "removeViewBox",
-              active: false,
-            },
-            {
-              name: "removeDimensions",
-              active: true,
-            },
-            {
-              name: "removeRasterImages",
-              active: true,
-            },
-            {
-              name: "reusePaths",
-              active: true,
-            },
-            {
-              name: "cleanupIDs",
-              active: false,
-            },
-            {
-              name: "prefixIds",
-              active: false,
-            },
-            {
-              name: "removeUselessDefs",
-              active: true,
-            },
-          ],
-        },
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `RRE Ventures`,
-        short_name: `rre`,
-        start_url: `/`,
-        background_color: `#000000`, //TODO set theme color
-        theme_color: `#000000`,
-        display: `minimal-ui`,
-        icon: "", //TODO set icon
-      },
-    },
-    {
-      resolve: `gatsby-plugin-tsconfig-paths`,
-      options: {
-        configFile: `${__dirname}/tsconfig.json`,
-        silent: true,
-      },
-    },
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-netlify",
-    "gatsby-plugin-image",
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
+    // TODO setup manifest
+    // {
+    //   resolve: "gatsby-plugin-manifest",
+    //   options: {
+    //     icon: "src/images/icon.png",
+    //   },
+    // },
+    {
+      resolve: "gatsby-plugin-root-import",
+      options: {
+        components: path.resolve("src/components"),
+        src: path.resolve("src"),
+        styles: path.resolve("src/assets/styles"),
+        pages: path.resolve("src/pages"),
+        fonts: path.resolve("src/assets/fonts"),
+        hooks: path.resolve("src/utils/hooks"),
+        images: path.resolve("src/assets/images"),
+        assets: path.resolve("src/assets"),
+        sections: path.resolve("src/sections"),
+        types: path.resolve("src/types"),
+        templates: path.resolve("src/templates"),
+        forms: path.resolve("src/components/forms"),
+        buttons: path.resolve("src/components/buttons"),
+        utils: path.resolve("src/utils"),
+      },
+    },
   ],
 }
 
 export default config
+
+// import type { GatsbyConfig } from "gatsby"
+
+// const config: GatsbyConfig = {
+//     "gatsby-plugin-styled-components",
+//     {
+//       resolve: `gatsby-plugin-typescript`,
+//       options: {
+//         isTSX: true, // defaults to false
+//         jsxPragma: `jsx`, // defaults to "React"
+//         allExtensions: true, // defaults to false
+//       },
+//     },
+//     {
+//       resolve: `gatsby-plugin-layout`,
+//       options: {
+//         component: require.resolve(`./src/components/Providers.tsx`),
+//       },
+//     },
+//     {
+//       resolve: `gatsby-plugin-svgr`,
+//       options: {
+//         prettier: true,
+//         svgo: true,
+//         memo: true,
+//         ref: true,
+//         svgoConfig: {
+//           plugins: [
+//             {
+//               name: "removeViewBox",
+//               active: false,
+//             },
+//             {
+//               name: "removeDimensions",
+//               active: true,
+//             },
+//             {
+//               name: "removeRasterImages",
+//               active: true,
+//             },
+//             {
+//               name: "reusePaths",
+//               active: true,
+//             },
+//             {
+//               name: "cleanupIDs",
+//               active: false,
+//             },
+//             {
+//               name: "prefixIds",
+//               active: false,
+//             },
+//             {
+//               name: "removeUselessDefs",
+//               active: true,
+//             },
+//           ],
+//         },
+//       },
+//     },
+//     {
+//       resolve: `gatsby-plugin-manifest`,
+//       options: {
+//         name: `RRE Ventures`,
+//         short_name: `rre`,
+//         start_url: `/`,
+//         background_color: `#000000`, //TODO set theme color
+//         theme_color: `#000000`,
+//         display: `minimal-ui`,
+//         icon: "", //TODO set icon
+//       },
+//     },
+//     {
+//       resolve: `gatsby-plugin-tsconfig-paths`,
+//       options: {
+//         configFile: `${__dirname}/tsconfig.json`,
+//         silent: true,
+//       },
+//     },
+//     "gatsby-plugin-react-helmet",
+//     "gatsby-plugin-sitemap",
+//     "gatsby-plugin-netlify",
+//     "gatsby-plugin-image",
+//     `gatsby-plugin-sharp`,
+//     `gatsby-transformer-sharp`,
+//   ],
+// }
+
+// export default config
--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -9,85 +9,74 @@ interface SEOProps {
   url?: string
 }
 const SEO: React.FC<SEOProps> = ({ description, lang, title, image, url }) => {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            image
-            siteUrl
-          }
-        }
-      }
-    `
-  )
+  return <div>SEO is not yet set up!</div>
 
-  const metaDescription = description || site.siteMetadata.description
-  const currentTitle = title || site.siteMetadata?.title
-  const currentImage = image || site.siteMetadata?.image
-  const currentURL = url || site.siteMetadata?.siteUrl
+  // static query was here
 
-  return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      title={currentTitle}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: currentTitle,
-        },
-        {
-          property: "og:image",
-          content: currentImage,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          property: `og:url`,
-          content: currentURL,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:image`,
-          content: currentImage,
-        },
-        {
-          name: `twitter:creator`,
-          content: ``,
-        },
-        {
-          name: `twitter:title`,
-          content: currentTitle,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-        {
-          name: "viewport",
-          content:
-            "width=device-width, initial-scale=1, maximum-scale=1, minimal-ui",
-        },
-      ]}
-    />
-  )
+  // const metaDescription = description || site.siteMetadata.description
+  // const currentTitle = title || site.siteMetadata?.title
+  // const currentImage = image || site.siteMetadata?.image
+  // const currentURL = url || site.siteMetadata?.siteUrl
+
+  // return (
+  //   <Helmet
+  //     htmlAttributes={{
+  //       lang,
+  //     }}
+  //     title={currentTitle}
+  //     meta={[
+  //       {
+  //         name: `description`,
+  //         content: metaDescription,
+  //       },
+  //       {
+  //         property: `og:title`,
+  //         content: currentTitle,
+  //       },
+  //       {
+  //         property: "og:image",
+  //         content: currentImage,
+  //       },
+  //       {
+  //         property: `og:description`,
+  //         content: metaDescription,
+  //       },
+  //       {
+  //         property: `og:type`,
+  //         content: `website`,
+  //       },
+  //       {
+  //         property: `og:url`,
+  //         content: currentURL,
+  //       },
+  //       {
+  //         name: `twitter:card`,
+  //         content: `summary`,
+  //       },
+  //       {
+  //         name: `twitter:image`,
+  //         content: currentImage,
+  //       },
+  //       {
+  //         name: `twitter:creator`,
+  //         content: ``,
+  //       },
+  //       {
+  //         name: `twitter:title`,
+  //         content: currentTitle,
+  //       },
+  //       {
+  //         name: `twitter:description`,
+  //         content: metaDescription,
+  //       },
+  //       {
+  //         name: "viewport",
+  //         content:
+  //           "width=device-width, initial-scale=1, maximum-scale=1, minimal-ui",
+  //       },
+  //     ]}
+  //   />
+  // )
 }
 
 export default SEO
