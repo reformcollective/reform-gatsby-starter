ea048dd6666a67a7cf349a50c73474cab93160fe
Robbie <robbie@reformcollective.com>
2022-04-25 10:29:45 -0600

    gatsby-config updates

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,4 +1,7 @@
-module.exports = {
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  //TODO set site metadata
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
@@ -70,34 +73,15 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-root-import",
-      options: {
-        components: path.join(__dirname, "src/components"),
-        src: path.join(__dirname, "src"),
-        styles: path.join(__dirname, "src/assets/styles"),
-        pages: path.join(__dirname, "src/pages"),
-        fonts: path.join(__dirname, "src/assets/fonts"),
-        hooks: path.join(__dirname, "src/utils/hooks"),
-        images: path.join(__dirname, "src/assets/images"),
-        assets: path.join(__dirname, "src/assets"),
-        sections: path.join(__dirname, "src/sections"),
-        types: path.join(__dirname, "src/types"),
-        templates: path.join(__dirname, "src/templates"),
-        forms: path.join(__dirname, "src/components/forms"),
-        buttons: path.join(__dirname, "src/components/buttons"),
-        utils: path.join(__dirname, "src/utils"),
-      },
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `RRE Ventures`,
         short_name: `rre`,
         start_url: `/`,
-        background_color: `#000000`,
+        background_color: `#000000`, //TODO set theme color
         theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: path.join(__dirname, "src/assets/images/global/logoMark.svg"),
+        icon: "", //TODO set icon
       },
     },
     {
@@ -115,3 +99,5 @@ module.exports = {
     `gatsby-transformer-sharp`,
   ],
 }
+
+export default config
