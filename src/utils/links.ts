1db0d65106d01805b6049486f82a6193fb465bfb
Robbie <robbiethewadley@gmail.com>
2022-08-23 13:03:33 -0600

    update links

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,7 +1,5 @@
 const links = {
-  marketShareEbook:
-    "https://www.galileo-ft.com/lp/grow-your-market-share-ebook/",
-  login: "https://console.galileo-ft.com/auth/login",
+  temporaryLink: "https://www.google.com",
 }
 
 export default links
