a094dbaba1643abde213d93e8a8ab6de3f58faab
Robbie <robbiethewadley@gmail.com>
2022-08-23 14:43:46 -0600

    documentation

--- src/components/TransitionLink.tsx
+++ src/components/TransitionLink.tsx
@@ -7,6 +7,12 @@ const allTransitions: Record<
   { inTimeline: gsap.core.Timeline[]; outTimeline: gsap.core.Timeline[] }
 > = {}
 
+/**
+ * register a transition that can be run with a transitionLink or by using loadPage
+ * @param name the name of the transition
+ * @param inTimeline the timeline to run before navigating
+ * @param outTimeline the timeline to run after navigating
+ */
 export const registerTransition = (
   name: string,
   inTimeline: gsap.core.Timeline,
@@ -19,6 +25,15 @@ export const registerTransition = (
   }
 }
 
+/**
+ * unregister a previously registered transition, such as when a transition component unmounts
+ *
+ * if inTimeline and outTimeline are not provided, all registered transitions with the given name will be unregistered
+ *
+ * @param name the name of the transition to unmount
+ * @param inTimeline if provided, only unregister this specific timeline
+ * @param outTimeline if provided, only unregister this specific timeline
+ */
 export const unregisterTransition = (
   name: string,
   inTimeline?: gsap.core.Timeline,
@@ -36,11 +51,22 @@ export const unregisterTransition = (
 }
 
 interface TransitionLinkProps {
+  /**
+   * the page to navigate to when clicked
+   */
   to: string
+  /**
+   * the transition to use when navigating
+   */
   transition?: string
   children: React.ReactNode
 }
 
+/**
+ * load a page, making use of the specified transition
+ * @param to page to load
+ * @param transition the transition to use
+ */
 export const loadPage = (to: string, transition?: string) => {
   if (!transition) {
     navigate(to)
@@ -67,6 +93,10 @@ export const loadPage = (to: string, transition?: string) => {
   }, entranceDuration * 1000)
 }
 
+/**
+ * a link that naviagates when clicked, using the specified transition
+ * @returns
+ */
 export function TransitionLink({
   to,
   transition = undefined,
