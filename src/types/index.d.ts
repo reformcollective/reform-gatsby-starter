36ddb05689207b038e4e751878681072ec0b54bb
Robbie <robbiethewadley@gmail.com>
2022-08-25 14:27:50 -0600

    types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,4 +1,19 @@
-declare global {}
+declare global {
+  interface Window {
+    /**
+     * special object that can be used to detect if running in safari
+     */
+    safari: unknown
+    /**
+     * special object that can be used to detect if running in chrome
+     */
+    chrome: unknown
+    /**
+     * special object that can be used to detect if running in firefox
+     */
+    netscape: unknown
+  }
+}
 
 export interface ScreenContextTypes {
   fullWidth: boolean
