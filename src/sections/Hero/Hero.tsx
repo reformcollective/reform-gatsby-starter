5a3d2f7edcf953f5db967fab950adb2491518163
Bubba Godinet <godinetvn@gmail.com>
2022-10-06 17:42:35 -0600

    test

---
+++ src/sections/Hero/Hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styled from "styled-components"
+
+export default function Hero() {
+  return (
+    <Wrapper>
+      Test
+    </Wrapper>
+  )
+}
+
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 52.083vw;
+`
