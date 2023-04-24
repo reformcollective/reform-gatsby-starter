9d4c350ebf058d56ce8ad298ec7aeda520544d10
Robbie <robbiethewadley@gmail.com>
2023-04-24 15:46:40 -0600

    update packages and drop react-scripts

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,4 +1,48 @@
-/// <reference types="react-scripts" />
+declare module "*.avif" {
+  const src: string
+  export default src
+}
+
+declare module "*.bmp" {
+  const src: string
+  export default src
+}
+
+declare module "*.gif" {
+  const src: string
+  export default src
+}
+
+declare module "*.jpg" {
+  const src: string
+  export default src
+}
+
+declare module "*.jpeg" {
+  const src: string
+  export default src
+}
+
+declare module "*.png" {
+  const src: string
+  export default src
+}
+
+declare module "*.webp" {
+  const src: string
+  export default src
+}
+
+declare module "*.svg" {
+  import * as React from "react"
+
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >
+
+  const src: string
+  export default src
+}
 
 declare global {
   interface Window {
@@ -17,4 +61,4 @@ declare global {
   }
 }
 
-export type Exclude = never
+export {}
