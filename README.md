221cbb232bb16e98d73741a513c63960414610fe
Robbie <robbiethewadley@gmail.com>
2023-05-01 16:08:58 -0600

    add focus state

--- README.md
+++ README.md
@@ -5,10 +5,9 @@ Setup:
 1. Clone and setup git for new repo (make sure to remove the old remote if needed)
 2. Update all existing packages with `npx npm-check-updates` (feel free to update the starter repository as well)
 3. Enable package update checks in `.github/workflows/call-check-updates.yml`
-4. Create a new file called `./github/dependabot.yml`
-5. Update `package.json` to include proper metadata for the new project
-6. Complete TODO items in `gatsby-config.ts`
-7. Add any typography to `src/styles/typography.css`
-8. Add any text styles to `src/styles/text.ts`
-9. Configure branch protections
-10. Init Library submodule: git submodule update --init --recursive
+4. Update `package.json` to include proper metadata for the new project
+5. Complete TODO items in `gatsby-config.ts`
+6. Add any typography to `src/styles/typography.css`
+7. Add any text styles to `src/styles/text.ts`
+8. Configure branch protections
+9. Init Library submodule: git submodule update --init --recursive
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,7 @@ import Scroll from "components/Scroll"
 import { useBackButton } from "library/Loader/TransitionUtils"
 import { useTrackPageReady } from "library/pageReady"
 import useCSSHeightVariables from "library/useCssHeightVariables"
-import styled from "styled-components"
+import styled, { createGlobalStyle, css } from "styled-components"
 
 interface LayoutProps {
   children: React.ReactNode
@@ -21,6 +21,7 @@ export default function Layout({ children }: LayoutProps) {
     <>
       <GreenTransition />
       <BlueTransition />
+      <GlobalStyle />
       <Scroll>
         <Header />
         <Main>{children}</Main>
@@ -33,3 +34,30 @@ export default function Layout({ children }: LayoutProps) {
 const Main = styled.main`
   overflow-x: hidden;
 `
+
+const globalCss = css`
+  * {
+    /* need this so that fonts match figma */
+    text-rendering: geometricprecision;
+  }
+
+  /* fixes for visbug */
+  vis-bug {
+    position: fixed;
+    z-index: var(--layer-1);
+  }
+
+  visbug-metatip,
+  visbug-handles,
+  visbug-ally {
+    position: absolute;
+    z-index: var(--layer-top);
+  }
+
+  /** restore default focus states for elements that need them */
+  *:focus-visible {
+    outline: 2px solid #f008;
+  }
+`
+
+const GlobalStyle = createGlobalStyle`${globalCss}`
