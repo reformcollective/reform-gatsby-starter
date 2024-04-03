76fc1aa80a94c7930cb1958f98c2ff12fd5bdffe
Robbie <robbiethewadley@gmail.com>
2024-04-03 13:12:44 -0600

    format

--- src/libraryConfig.ts
+++ src/libraryConfig.ts
@@ -1,4 +1,4 @@
-import { Config, defaultConfig } from "library/defaultConfig"
+import { type Config, defaultConfig } from "library/defaultConfig"
 
 /**
  * The transition names that can be used in the page transition
